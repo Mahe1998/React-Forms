3fe29232c1130a0039c5f554383e99273a9a738a
Mahe1998 <87573259+Mahe1998@users.noreply.github.com>
2022-03-23 22:32:32 +0530

    Final.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 
 function App() {
+  const [name, setName] = React.useState("");
+  const [headingText, setHeading] = React.useState("");
+
+  function handleChange(event) {
+    //console.log(event.target.value);
+    setName(event.target.value);
+  }
+
+  function handleClick() {
+    //console.log("Clicked");
+    setHeading(name);
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {headingText}</h1>
+      <input
+        type="text"
+        placeholder="What's your name?"
+        onChange={handleChange}
+        value={name}
+      />
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 }
